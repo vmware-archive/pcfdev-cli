007363f0796a14121ccc9a1ffd3d9b654da41ffb
Keaty Gross <kgross@pivotal.io>
2016-06-10 16:52:36 -0400

    Convert Free and Total memory to MB
    [Finishes #120615439]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- system/system.go
+++ system/system.go
@@ -2,6 +2,8 @@ package system
 
 import "github.com/cloudfoundry/gosigar"
 
+const BYTES_IN_MEGABYTE = 1048576
+
 //go:generate mockgen -package mocks -destination mocks/fs.go github.com/pivotal-cf/pcfdev-cli/system FS
 type FS interface {
 	Read(path string) ([]byte, error)
@@ -17,7 +19,7 @@ func (s *System) FreeMemory() (uint64, error) {
 	if err != nil {
 		return 0, err
 	}
-	return mem.ActualFree, nil
+	return mem.ActualFree / BYTES_IN_MEGABYTE, nil
 }
 
 func (s *System) TotalMemory() (uint64, error) {
@@ -26,5 +28,5 @@ func (s *System) TotalMemory() (uint64, error) {
 	if err != nil {
 		return 0, err
 	}
-	return mem.Total, nil
+	return mem.Total / BYTES_IN_MEGABYTE, nil
 }

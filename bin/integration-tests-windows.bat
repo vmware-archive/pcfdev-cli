fc60fba07e7d294872fac93316955d26118efee2
Anthony Emengo <aemengo@pivotal.io>
2016-06-14 11:59:53 -0400

    Migrate main_test into integration folder
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- bin/integration-tests-windows.bat
+++ bin/integration-tests-windows.bat
@@ -1,2 +1,2 @@
 go install github.com\pivotal-cf\pcfdev-cli\vendor\github.com\onsi\ginkgo\ginkgo
-ginkgo "%*" -noColor -skipPackage="integration" -r %~dp0\..
+ginkgo "%*" -noColor -r %~dp0\..\integration
